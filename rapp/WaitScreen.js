ac928b039e9b48b892a881ce0a0eb28874f8b9e9
James Echmalian <ech@ech.net>
2020-06-27 10:26:32 -0400

    WaitScreen

---
+++ rapp/WaitScreen.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import "./WaitScreen.css"
+
+export const WaitScreen = () => (
+  <div className="WaitScreen">
+    <div className="spinner">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+  </div>
+)
+
+export default WaitScreen
