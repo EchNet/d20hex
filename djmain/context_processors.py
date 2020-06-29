ed7907711104e9184add25e9979d8b4280d38eb7
James Echmalian <ech@ech.net>
2020-06-29 15:33:24 -0400

    Continue building out the config mechanism.

--- djmain/context_processors.py
+++ djmain/context_processors.py
@@ -3,7 +3,16 @@ from django.conf import settings
 
 def context_settings(request=None):
   return {
-      "DEBUG": settings.DEBUG,
-      "DEMO": settings.DEMO,
-      "STAGING": settings.STAGING,
+      "DEBUG":
+      settings.DEBUG,
+      "configuration": [
+          {
+              "key": "DEBUG",
+              "value": "1" if settings.DEBUG else ""
+          },
+          {
+              "key": "maxCampaignsPerPlayer",
+              "value": 4
+          },
+      ]
   }
--- rapp/config.js
+++ rapp/config.js
@@ -1,5 +1,9 @@
-export const config = (key) => {
-  if (key === "maxCampaignsPerPlayer")
-    return 4;
+export const config = (key, dflt=undefined) => {
+  const elements = document.querySelectorAll(`config[key="${key}"]`)
+  if (elements && elements.length) {
+    return elements[0].getAttribute("value")
+  }
+  return dflt;
 }
+
 export default config
