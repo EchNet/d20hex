15f303fd36ed6114c4eae9456880cc2a72121b0b
James Echmalian <ech@ech.net>
2020-07-06 13:51:17 -0400

    Update onboarding page.

--- rapp/Onboarding.js
+++ rapp/Onboarding.js
@@ -2,57 +2,48 @@ import * as React from "react"
 import { connect } from 'react-redux';
 
 import actions from "./actions"
-import FatHeader from "./FatHeader"
+import Menu from "./Menu"
 import Modal from "./Modal"
+import SingleTextValueForm from "./SingleTextValueForm"
 import "./Onboarding.css"
 
 
 export class Onboarding extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      playerNameInput: ""
-    }
-  }
   render() {
     return (
       <div className="Onboarding">
-        <FatHeader/>
-        <Modal modal={false}>
-          <form onSubmit={(event) => this.handlePlayerFormSubmit(event)}>
-            <div className="titlebar">
-              Welcome to <span className="Logo">d20hex</span>!
-            </div>
-            <div className="body">
-              What would you like to be called?<br/>
-              <small>
-                (This is your name as a player, not the name of your character.  You can
-                change it at any time.)
-              </small>
-            </div>
-            <div className="body">
-              <input onChange={(event) => this.handlePlayerNameChange(event)}
-                  placeholder="Enter player name"/>
-            </div>
-            <div className="footer">
-              <input type="submit" disabled={this.state.playerNameInput.length ? "" : "disabled"}/>
-            </div>
-          </form>
+        <header>
+          <div className="leftSide">
+            <img className="logo" src="/static/img/logo.png" height="120" alt="d20hex"/>
+          </div>
+          <div className="rightSide">
+            <Menu label={this.props.userName}/>
+          </div>
+        </header>
+        <Modal>
+          <div className="titlebar">
+            Welcome to <span className="Logo">d20hex</span>!
+          </div>
+          <div className="body">
+            What would you like to be called?<br/>
+            <small>
+              (This is your name as a player, not the name of your character.  You can
+              change it at any time.)
+            </small>
+          </div>
+          <div className="body">
+            <SingleTextValueForm placeholder="Enter player name"
+                onSubmit={(input) => this.handlePlayerFormSubmit(input)}/>
+          </div>
         </Modal>
       </div>
     )
   }
-  handlePlayerNameChange(event) {
-    this.setState({ playerNameInput: event.target.value })
-  }
-  handlePlayerFormSubmit(event) {
-    event.preventDefault()
-    if (this.state.playerNameInput.length) {
-      this.props.dispatch({
-        type: actions.CREATE_PLAYER,
-        props: { name: this.state.playerNameInput }
-      })
-    }
+  handlePlayerFormSubmit(input) {
+    this.props.dispatch({
+      type: actions.CREATE_PLAYER,
+      props: { name: input }
+    })
   }
 }
 
