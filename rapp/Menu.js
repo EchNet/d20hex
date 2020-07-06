649687d4dc865981ce0c01ec4bbc39a3f31123b7
James Echmalian <ech@ech.net>
2020-07-06 10:34:55 -0400

    Work on menus.

---
+++ rapp/Menu.js
@@ -0,0 +1,48 @@
+import * as React from "react"
+
+import "./Menu.css"
+
+
+export class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dropdownShown: false
+    }
+  }
+  render() {
+    return (
+      <div className={"Menu" + (this.props.right ? " right" : "")}>
+        <button onMouseEnter={() => this.toggleDropdown()}
+            onMouseLeave={() => this.toggleDropdown() }>
+          {this.props.label}
+        </button>
+        { this.state.dropdownShown && (
+          <ul onMouseEnter={() => this.toggleDropdown()}
+              onMouseLeave={() => this.toggleDropdown()}
+              onClick={() => this.toggleDropdown()}>
+            {this.props.children}
+          </ul>
+        )}
+      </div>
+    )
+  }
+  toggleDropdown() {
+    this.setState((oldState) => ({ dropdownShown: !oldState.dropdownShown }))
+  }
+}
+
+export class MenuItem extends React.Component {
+  render() {
+    return (
+      <li className="MenuItem" onClick={(event) => this.handleClick(event)}>
+        {this.props.children}
+      </li>
+    )
+  }
+  handleClick(event) {
+    this.props.onClick && this.props.onClick(event)
+  }
+}
+
+export default Menu
