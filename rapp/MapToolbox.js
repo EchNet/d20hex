5bebd60a39ccdd63238fbb59ad41dfa86655e0c1
James Echmalian <ech@ech.net>
2020-07-26 10:07:26 -0400

    PaletteGroup arrives.

--- rapp/MapToolbox.js
+++ rapp/MapToolbox.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import actions from "./actions"
 import config from "./config"
+import PaletteGroup from "./PaletteGroup"
 import "./MapToolbox.css"
 
 let DEBUG = config("DEBUG");
@@ -11,22 +12,21 @@ export class MapToolbox extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      bgFillStyles: [
-        "#848484", "#bda878", "#449944", "#7686ee", "white"
+      bgToolChoices: [
+        "bg|#848484", "bg|#bda878", "bg|#449944", "bg|#7686ee", "bg|white"
       ],
-      bgFillStyleIndex: 0,
-      counterFillStyles: [ "black", "#800", "#808" ],
-      counterFillStyleIndex: 0,
-      tokenFillStyles: [
-        "url(http://ech.net/img/d20hex/evilbaron.png)",
-        "url(http://ech.net/img/d20hex/ghoul.png)",
-        "url(http://ech.net/img/d20hex/girl.png)",
-        "url(http://ech.net/img/d20hex/knight.png)",
-        "url(http://ech.net/img/d20hex/lurch.png)",
-        "url(http://ech.net/img/d20hex/necrogirl.png)",
-        "url(http://ech.net/img/d20hex/wizard.png)"
+      counterToolChoices: [
+        "counter|black", "counter|#b00", "counter|#808"
       ],
-      tokenFillStyleIndex: 0,
+      tokenToolChoices: [
+        "token|url(http://ech.net/img/d20hex/evilbaron.png)",
+        "token|url(http://ech.net/img/d20hex/ghoul.png)",
+        "token|url(http://ech.net/img/d20hex/girl.png)",
+        "token|url(http://ech.net/img/d20hex/knight.png)",
+        "token|url(http://ech.net/img/d20hex/lurch.png)",
+        "token|url(http://ech.net/img/d20hex/necrogirl.png)",
+        "token|url(http://ech.net/img/d20hex/wizard.png)"
+      ]
     }
   }
   render() {
@@ -43,119 +43,28 @@ export class MapToolbox extends React.Component {
           </div>
         )}
         <hr></hr>
-        { this.renderBgTool() }
-        <hr></hr>
-        { this.renderCounterTool() }
-        <hr></hr>
-        { this.renderTokenTool() }
+        <PaletteGroup paletteValue={this.props.selectedTool} 
+            choices={this.state.bgToolChoices}
+            onSelect={(value) => this.selectNewTool(value)}/>
+        <PaletteGroup paletteValue={this.props.selectedTool} 
+            label={this.props.counterValue}
+            choices={this.state.counterToolChoices}
+            onSelect={(value) => this.selectNewTool(value)}/>
+        <PaletteGroup paletteValue={this.props.selectedTool} 
+            choices={this.state.tokenToolChoices}
+            onSelect={(value) => this.selectNewTool(value)}/>
       </div>
     )
   }
-  renderBgTool() {
-    const currentFillStyle = this.state.bgFillStyles[this.state.bgFillStyleIndex]
-    const toolName = `bg|${currentFillStyle}`
-    return (
-      <div className="tool-wrapper">
-        <div className="left-arrow" onClick={() => this.handleBgToolRotate(-1)}>{"<"}</div>
-        <div className={this.classifyTool("bg", currentFillStyle)}
-            onClick={(event) => this.selectNewTool(toolName)}
-            style={{ backgroundColor: currentFillStyle }}/>
-        <div className="right-arrow" onClick={() => this.handleBgToolRotate(1)}>{">"}</div>
-      </div>
-    )
-  }
-  handleBgToolRotate(incr) {
-    const nFillStyles = this.state.bgFillStyles.length;
-    const newFillStyleIndex = (this.state.bgFillStyleIndex + nFillStyles + incr) % nFillStyles;
-    this.setState({ bgFillStyleIndex: newFillStyleIndex })
-    if (this.props.selectedTool.startsWith("bg")) {
-      const newFillStyle = this.state.bgFillStyles[newFillStyleIndex]
-      const toolName = `bg|${newFillStyle}`
-      this.selectNewTool(toolName)
-    }
-  }
-  renderCounterTool() {
-    const currentFillStyle = this.state.counterFillStyles[this.state.counterFillStyleIndex]
-    const digits = this.props.counterValue.toString().length;
-    return (
-      <div className="tool-wrapper">
-        <div className="left-arrow" onClick={() => this.handleCounterToolRotate(-1)}>{"<"}</div>
-        <div className={this.classifyTool("counter", currentFillStyle)}
-            onClick={(event) => this.selectNewTool(`counter:${currentFillStyle}`)}
-            style={{
-                color: "white",
-                backgroundColor: currentFillStyle,
-                width: "28px",
-                height: "28px",
-                lineHeight: `${25 - digits*2}px`,
-                borderRadius: "50%",
-                fontSize: `${23 - digits*2}px`,
-                textAlign: "center"
-            }}>
-          { this.props.counterValue }
-        </div>
-        <div className="right-arrow" onClick={() => this.handleCounterToolRotate(1)}>{">"}</div>
-      </div>
-    )
-  }
-  handleCounterToolRotate(incr) {
-    const nFillStyles = this.state.counterFillStyles.length;
-    const newFillStyleIndex = (this.state.counterFillStyleIndex + nFillStyles + incr) % nFillStyles;
-    this.setState({ counterFillStyleIndex: newFillStyleIndex })
-    if (this.props.selectedTool && this.props.selectedTool.startsWith("counter")) {
-      const newFillStyle = this.state.counterFillStyles[newFillStyleIndex]
-      const toolName = `counter|${newFillStyle}`;
-      this.selectNewTool(toolName)
-    }
-  }
-  renderTokenTool() {
-    const currentFillStyle = this.state.tokenFillStyles[this.state.tokenFillStyleIndex]
-    const digits = this.props.counterValue.toString().length;
-    return (
-      <div className="tool-wrapper">
-        <div className="left-arrow" onClick={() => this.handleTokenToolRotate(-1)}>{"<"}</div>
-        <div className={this.classifyTool("token", currentFillStyle)}
-            onClick={(event) => this.selectNewTool(`token:${currentFillStyle}`)}
-            style={{
-                backgroundImage: currentFillStyle,
-                backgroundSize: "cover",
-                width: "32px",
-                height: "32px",
-                lineHeight: `${28 - digits*2}px`,
-                borderRadius: "50%"
-            }}>
-        </div>
-        <div className="right-arrow" onClick={() => this.handleTokenToolRotate(1)}>{">"}</div>
-      </div>
-    )
-  }
-  handleTokenToolRotate(incr) {
-    const nFillStyles = this.state.tokenFillStyles.length;
-    const newFillStyleIndex = (this.state.tokenFillStyleIndex + nFillStyles + incr) % nFillStyles;
-    this.setState({ tokenFillStyleIndex: newFillStyleIndex })
-    if (this.props.selectedTool.startsWith("token")) {
-      const newFillStyle = this.state.tokenFillStyles[newFillStyleIndex]
-      const toolName = `token|${newFillStyle}`;
-      this.selectNewTool(toolName)
-    }
-  }
-  classifyTool(name1, name2 = null) {
-    const toolName = name2 ? `${name1}|${name2}` : name1;
-    let className = `tool ${name1}`;
-    if (name2 == "white") {
-      className = `${className} white`;
-    }
-    if (this.props.selectedTool.startsWith(name1)) {
-      className = `${className} selected`;
-    }
-    return className;
+  classifyTool(toolName) {
+    const isSelected = this.props.selectedTool && this.props.selectedTool == toolName;
+    return `tool ${toolName}${isSelected ? " selected" : ""}`;
   }
   selectNewTool(newSelectedTool) {
-    this.props.dispatch({ type: actions.SELECT_TOOL, tool: newSelectedTool })
+    this.props.dispatch({ type: actions.SELECT_TOOL, data: newSelectedTool })
   }
 }
 const mapState = (state) => {
   return Object.assign({}, state);
 }
 export default connect(mapState)(MapToolbox)
-
---
+++ rapp/PaletteGroup.js
@@ -0,0 +1,64 @@
+import * as React from "react"
+
+import "./PaletteGroup.css"
+
+export class PaletteGroup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      choices: props.choices.slice(),
+      forceClose: false
+    }
+  }
+  render() {
+    const isSelected = !!this.state.choices.find((ele) => ele == this.props.paletteValue)
+    const jsx = (
+      <div className={`PaletteGroup${isSelected ? " selected" : ""}`}>
+        <div className="slideOut">
+          { !this.state.forceClose && this.state.choices.map((ele) => this.renderCell(ele)) }
+          { !!this.state.forceClose && this.renderCell(this.state.choices[0]) }
+        </div>
+      </div>
+    )
+    return jsx;
+  }
+  renderCell(value) {
+    const valueParts = value.split("|")
+    const classes = ["cell"].concat(this.extraCellClasses(valueParts))
+    const styles = this.parseStyles(valueParts)
+    const label = this.props.label || ""; 
+    return (
+      <div key={value} className={classes.join(" ")} style={styles}
+          onClick={(event) => this.handleCellClick(value)}>{label}</div>
+    )
+  }
+  extraCellClasses(valueParts) {
+    return valueParts.length > 1 && valueParts[1] == "white" ? ["white"] : []
+  }
+  parseStyles(valueParts) {
+    const styles = {}
+    if (valueParts.length > 1) {
+      if (valueParts[1].startsWith("url(")) {
+        styles.background = valueParts[1];
+        styles.backgroundSize = "cover";
+      }
+      else {
+        styles.backgroundColor = valueParts[1]
+      }
+    }
+    return styles;
+  }
+  handleCellClick(value) {
+    this.props.onSelect && this.props.onSelect(value)
+    this.setState({ choices: this.rearrangeChoices(value), forceClose: true })
+    setTimeout(() => { this.setState({ forceClose: false }) }, 200)
+  }
+  rearrangeChoices(value) {
+    let newChoices = this.state.choices.filter((ele) => ele != value)
+    newChoices.unshift(value);
+    return newChoices;
+  }
+}
+
+export default PaletteGroup;
+
